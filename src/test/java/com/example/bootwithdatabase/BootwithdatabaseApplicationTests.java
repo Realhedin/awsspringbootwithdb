f94d279a98827aca6edc88e6e8a47cb717360e25
Dmitrii_Korolev <korwork@mail.ru>
2019-11-26 12:24:57 -0500

    standard structure

---
+++ src/test/java/com/example/bootwithdatabase/BootwithdatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootwithdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootwithdatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
